e0254be21154187cb4f66dc330c233fb3cafae3b
Frank Lyaruu <f.lyaruu@quin.nl>
2023-07-31 09:46:00 +0200

    major refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,45 @@
-pub mod panda_client;
+pub mod panda_subscriber;
+pub mod panda_publisher;
 
+use std::fmt::Display;
+
+use thiserror::Error;
+
+#[derive(Debug,Error)]
+pub struct RedPandaError(pub String);
+
+impl Display for RedPandaError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
+mod base64_option {
+    use base64::Engine;
+    use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
+    use serde::{Serialize, Deserialize};
+    use serde::{Deserializer, Serializer};
+
+    pub fn serialize<S: Serializer>(v: &Option<Vec<u8>>, s: S) -> Result<S::Ok, S::Error> {
+        // base64::encode(input)
+        let base64 = v.as_ref().map(|element| BASE64_STANDARD.encode(element));
+        // let base64 = match v {
+
+        //     Some(v) => Some(BASE64_STANDARD.encode(v)),
+        //     None => None,
+        // };
+        <Option<String>>::serialize(&base64, s)
+    }
+    
+    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Option<Vec<u8>>, D::Error> {
+        let base64 = <Option<String>>::deserialize(d)?;
+        match base64 {
+            Some(v) => {
+                BASE64_STANDARD.decode(v.as_bytes())
+                    .map(Some)
+                    .map_err(serde::de::Error::custom)
+            },
+            None => Ok(None),
+        }
+    }
+}
--- src/panda_subscriber.rs
+++ src/panda_subscriber.rs
@@ -1,14 +1,13 @@
-use std::{collections::HashMap, str::from_utf8, vec};
 
+use std::{collections::HashMap, str::from_utf8, vec};
 use log::debug;
-use serde::Deserializer;
-use serde::{Serialize, Deserialize, ser::SerializeMap};
+use serde::{Serialize, Deserialize};
 
 const DEBUG: bool = true;
 
-use base64::Engine;
-use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
 use simplehttp::simplehttp::SimpleHttpClient;
+
+use crate::{base64_option, RedPandaError};
 
 #[derive(Serialize)]
 pub struct Consumer<'a> {
@@ -23,8 +22,7 @@ pub struct Consumer<'a> {
     consumer_request_timeout_ms: &'a str,
 }
 
-#[derive(Debug)]
-pub struct RedPandaError(pub String);
+
 
 
 impl<'a> Consumer<'a> {
@@ -34,7 +32,7 @@ impl<'a> Consumer<'a> {
 }
 
 #[derive(Serialize)]
-pub struct CommitState {
+struct CommitState {
     topics: HashMap<String,HashMap<u16,u64>>
 }
 #[derive(Serialize)]
@@ -45,11 +43,11 @@ pub struct CommitPartition {
 }
 
 impl CommitState {
-    pub fn create()->CommitState {
+    fn create()->CommitState {
         CommitState { topics: HashMap::new() }
     }
 
-    pub fn process_record(&mut self, record: &Record) {
+    fn process_record(&mut self, record: &Record) {
         let topic = self.topics.get_mut(&record.topic);
         match topic {
             None => {
@@ -63,7 +61,7 @@ impl CommitState {
         }
     }
 
-    pub fn partition_list(&self)->Vec<CommitPartition> {
+    fn partition_list(&self)->Vec<CommitPartition> {
         let mut result: Vec<CommitPartition> = vec![];
         for (topic,partitions) in &self.topics {
             for (partition,offset) in partitions.iter() {
@@ -71,6 +69,10 @@ impl CommitState {
             }
         }
         result
+    }
+
+    fn clear(&mut self) {
+        self.topics.clear()
     }
 }
 #[derive(Deserialize,Serialize)]
@@ -95,61 +97,34 @@ pub struct Record {
     pub offset: u64
 }
 
-#[derive(Deserialize)]
-pub struct PublishRecord {
-    pub key: Option<Vec<u8>>,
-    #[serde(with="base64_option")]
-    pub value: Option<Vec<u8>>,
-}
-
-impl PublishRecord {
-    pub fn from_string(message: String)->Self {
-        Self { key: None, value: Some(message.into_bytes()) }
-    }
-
-    pub fn from_bytes(value: Option<&[u8]>)->Self {
-        PublishRecord { key: None, value: value.map(|f|f.to_vec()) }
-    }
-}
-
-#[derive(Serialize)]
-pub struct PublishRecordList {
-    pub records: Vec<PublishRecord>
-}
-
-impl PublishRecordList {
-    pub fn from_string(message: String)->Self {
-        PublishRecordList{records: vec![PublishRecord::from_string(message)]}
-    } 
-}
-
-pub struct RedPandaClient {
+pub struct Subscriber {
     inital_url: String,
     group: String,
     consumer_response: Option<ConsumerResponse>,
     client: Box<dyn SimpleHttpClient>,
-
+    commit_state: CommitState,
 }
 
-impl RedPandaClient {
-    pub fn new(http_client: Box<dyn SimpleHttpClient>, inital_url: &str, group: &str)->Result<RedPandaClient,RedPandaError> {
-        let mut client = RedPandaClient {inital_url: inital_url.to_owned(), group: group.to_owned(), consumer_response: Option::None, client: http_client};
+impl Subscriber {
+    pub fn new(http_client: Box<dyn SimpleHttpClient>, inital_url: &str, group: &str)->Result<Self,RedPandaError> {
+        let mut client = Self {inital_url: inital_url.to_owned(), group: group.to_owned(), consumer_response: Option::None, client: http_client, commit_state: CommitState::create()};
         let consumer = Consumer::create();
         let body = serde_json::to_vec(&consumer)
             .map_err(|_| RedPandaError("Error serializing JSON request".to_owned()))?
         ;
         let url = format!("{}consumers/{}", client.inital_url, client.group);
-        let mut headers = vec![("Content-Type","application/vnd.kafka.v2+json")];
+        let headers = vec![("Content-Type","application/vnd.kafka.v2+json")];
         if DEBUG {
             debug!("Initializing using url: {}\nBody:\n{}",url,serde_json::to_string_pretty(&consumer).unwrap());
             debug!("Headers: {:?}",headers);
         }
-        let result = client.client.post(&url, &mut headers, &body).map_err(|e| RedPandaError(format!("error creating consumer: {:?}",e)))?;
+        let result = client.client.post(&url, &headers, &body).map_err(|e| RedPandaError(format!("error creating consumer: {:?}",e)))?;
         if DEBUG {
             debug!("Result text:\n{}", from_utf8(result.as_slice()).map_err(|_| RedPandaError("Issues creating utf8".to_owned()))?);
         }
 
         client.consumer_response = Some(serde_json::from_slice(&result).map_err(|_| RedPandaError("Error parsing JSON Red Panda reply".to_owned()))?);
+
         Ok(client)
     }
 
@@ -161,10 +136,14 @@ impl RedPandaClient {
         if DEBUG {
             debug!("Registering topic using url: {}\nBody:\n{}",url,serde_json::to_string_pretty(&subscr).unwrap())
         }
-        let _ = self.client.post(&url, &vec![("Content-Type","application/vnd.kafka.v2+json")], &body).map_err(|e| RedPandaError(format!("error registering topic: {:?}",e)))?;
+        let _ = self.client.post(&url, &[("Content-Type","application/vnd.kafka.v2+json")], &body).map_err(|e| RedPandaError(format!("error registering topic: {:?}",e)))?;
         Ok(())
     }
 
+    pub fn process_record(&mut self, record: &Record) {
+        self.commit_state.process_record(record);
+        self.commit_state.clear();
+    }
     // pub fn poll_thing<'a, T>(&mut self, bytecount: i32) -> Result<Vec<T>,RedPandaError> where for<'de> T: Deserialize<'de> + 'a {
     //     let l: Vec<Record> = self.poll( bytecount)?;
     //     let xx: Vec<T> = l.iter().filter_map(f) map(|f| serde_json::from_slice(&f.value[..])).collect();
@@ -177,7 +156,7 @@ impl RedPandaClient {
         }
                 // .header("Accept", "application/vnd.kafka.binary.v2+json")
 
-        let records = self.client.get(&url,&vec![("Accept", "application/vnd.kafka.binary.v2+json")])
+        let records = self.client.get(&url,&[("Accept", "application/vnd.kafka.binary.v2+json")])
             .map_err(|e| RedPandaError(format!("error polling: {:?}",e)))?;
         if DEBUG {
             let text = String::from_utf8(records.clone()).unwrap();
@@ -190,18 +169,10 @@ impl RedPandaClient {
         Ok(parsed)
     }
     
-    // TODO: return list of OffsetRecordSent
-    pub fn publish(&mut self, topic: String, record: PublishRecordList)->Result<(), RedPandaError> {
-        let url = format!("{}topics/{}",self.inital_url,topic);
-        let headers = vec![("Content-Type","application/vnd.kafka.binary.v2+json")];
-        let l = serde_json::to_vec(&record).map_err(|_| RedPandaError("error serializing publish".to_owned()))?;
-        let _reply = self.client.post(&url, &headers, &l[..])
-            .map_err(|e| RedPandaError(format!("error publishing: {:?}",e)))?;
-        Ok(())
-    }
 
-    pub fn commit_state(&mut self, state: &CommitState) ->  Result<(), RedPandaError> {
-        let partitions = state.partition_list();
+
+    pub fn commit_state(&mut self) ->  Result<(), RedPandaError> {
+        let partitions = self.commit_state.partition_list();
         let commits = HashMap::from([("partitions".to_owned(),partitions)]);
         let body = serde_json::to_vec(&commits ).map_err(|_| RedPandaError("Error serializing commit state".to_owned()))?;
         // let value = from_utf8(&body).map_err(|_| RedPandaError("UTF8 error".to_owned()))?;
@@ -209,65 +180,19 @@ impl RedPandaClient {
         if DEBUG {
             debug!("Committing to url:{}\nBody:\n{}",url,serde_json::to_string_pretty(&commits).unwrap());
         }
-        let _ = self.client.post(&url, &vec![], &body)
+        let _ = self.client.post(&url, &[], &body)
             .map_err(|e| RedPandaError(format!("error commiting state: {:?}",e)))
         ;
+        // I guess clear the commit state?
         Ok(())
     }
-
 
     pub fn instance_id(&self) -> Option<String> {
         Some(self.consumer_response.as_ref().unwrap().instance_id.clone())
     }
 }
 
-impl Serialize for PublishRecord {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer {
-        let field_count = self.key.as_ref().map(|_|1).unwrap_or(0) + 
-            self.value.as_ref().map(|_|1).unwrap_or(0);
 
-        // todo!()
-        let mut map = serializer.serialize_map(Some(field_count))?;
-        if self.key.is_some() {
-            map.serialize_entry("key",&BASE64_STANDARD.encode(self.key.as_ref().unwrap()))?;
-        }
-        if self.value.is_some() {
-            map.serialize_entry("value",&BASE64_STANDARD.encode(self.value.as_ref().unwrap()))?;
-        }
-        map.end()
-    }
-}
-
-mod base64_option {
-    use base64::Engine;
-    use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
-    use serde::{Serialize, Deserialize};
-    use serde::{Deserializer, Serializer};
-
-    pub fn serialize<S: Serializer>(v: &Option<Vec<u8>>, s: S) -> Result<S::Ok, S::Error> {
-        // base64::encode(input)
-        let base64 = match v {
-
-            Some(v) => Some(BASE64_STANDARD.encode(v)),
-            None => None,
-        };
-        <Option<String>>::serialize(&base64, s)
-    }
-    
-    pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Option<Vec<u8>>, D::Error> {
-        let base64 = <Option<String>>::deserialize(d)?;
-        match base64 {
-            Some(v) => {
-                BASE64_STANDARD.decode(v.as_bytes())
-                    .map(|v| Some(v))
-                    .map_err(|e| serde::de::Error::custom(e))
-            },
-            None => Ok(None),
-        }
-    }
-}
 
 #[cfg(test)]
 #[cfg(feature = "reqwest")]
